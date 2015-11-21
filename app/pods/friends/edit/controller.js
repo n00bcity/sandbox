4683fb1463dadd2150fcb971b70af52725522b4c
n00bcity <i1140100@trbvm.com>
2015-11-21 14:30:35 -0500

    styling
    added styling elements and article resource

--- app/pods/friends/edit/controller.js
+++
@@ -1,4 +0,0 @@
-//CTL friends.edit
-import Ember from 'ember';
-
-export default Ember.Controller.extend({});
--- tests/unit/pods/articles/route-test.js
+++ tests/unit/pods/articles/route-test.js
@@ -1,12 +1,11 @@
 import { moduleFor, test } from 'ember-qunit';
 
-moduleFor('controller:friends/new', 'Unit | Controller | friends/new', {
+moduleFor('route:articles', 'Unit | Route | articles', {
   // Specify the other units that are required for this test.
   // needs: ['controller:foo']
 });
 
-// Replace this with your real tests.
 test('it exists', function(assert) {
-  let controller = this.subject();
-  assert.ok(controller);
+  let route = this.subject();
+  assert.ok(route);
 });
